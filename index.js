844422c0ffc74cfa0cf8c527e86647a609e89f1e
philip martinez <martinez.philip00@gmail.com>
2022-05-05 14:34:48 -0500

    commit tree 67df3e32e29db0b8b1f7a8842e35f537d370cce8

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const leastFavoriteCustomer = 'Some Initial Value'
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    return customerName = 'BOB'
+}
+
+console.log(customerName)
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob'
+}
+
+console.log(bestCustomer)
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob'
+}
+
+console.log(bestCustomer)
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Assignment to constant variable'
+}
+
+console.log(leastFavoriteCustomer)
